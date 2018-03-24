93524b9ca10e3c00e85e1f282dd41e9358f31df1
liuruinan8 <767136895@qq.com>
2018-03-24 09:35:26 +0800

    Initial Commit
    Signed-off-by: liuruinan8 <767136895@qq.com>

---
+++ Example/Pods/Target Support Files/TalkingDataSwift/TalkingDataSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TalkingDataSwift : NSObject
+@end
+@implementation PodsDummy_TalkingDataSwift
+@end
---
+++ Example/Pods/Target Support Files/TalkingDataSwift/TalkingDataSwift-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "TalkingData.h"
+
+FOUNDATION_EXPORT double TalkingDataSwiftVersionNumber;
+FOUNDATION_EXPORT const unsigned char TalkingDataSwiftVersionString[];
+
